a3c5ef90596577e554e13b2a896c191e1db6d1a1
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-29 20:23:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+const tableBody = document.getElementById("output");
+tableBody.innerHTML = `
+<tr><td colspan="2">Loading...</td></tr>
+<tr><td colspan="2">Loading...</td></tr>
+<tr><td colspan="2">Loading...</td></tr>
+`
+
+function promise1() {
+	return new Promise((resolve, reject)=>{
+			let time = Math.random()
+		setTimeout(()=>{
+			resolve(time*3)
+		},time*3000)
+	})
+	
+}
+
+function promise2() {
+	return new Promise((resolve, reject)=>{
+			let time = Math.random()
+		setTimeout(()=>{
+			resolve(time*3)
+		},time*3000)
+	})
+	
+}
+
+function promise3() {
+	return new Promise((resolve, reject)=>{
+			let time = Math.random();
+		setTimeout(()=>{
+			resolve(time*3)
+		}, time*3000)
+	})
+	
+}
+
+Promise.all([promise1(), promise2(), promise3()]).then((data)=>{
+	tableBody.innerHTML=`
+	<tr><td>Promise 1</td><td>${data[0].toFixed(0)}</td></tr>
+<tr><td>Promise 2</td><td>${data[1].toFixed(0)}</td></tr>
+<tr><td>Promise 3</td><td>${data[2].toFixed(0)}</td></tr>
+<tr><td><b>Total</b></td><td>${(data[0]+data[1]+data[2]).toFixed(3)}</td></tr>
+`
+	console.log(data);
+}).catch((e)=>{
+	console.log(e);
+})
+
+
+// dddddd
